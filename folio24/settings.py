17ab865b18d01acd0ed13da673a201addf66eaf8
James Pasko <jbpasko@gmail.com>
2013-03-27 22:04:23 -0700

    domains

--- folio24/settings.py
+++ folio24/settings.py
@@ -110,8 +110,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    #'middleware.custom.ParseURLs',
-    #'middleware.custom.RedirectToCustomDomain',
+    'middleware.custom.ParseURLs',
+    'middleware.custom.RedirectToCustomDomain',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
