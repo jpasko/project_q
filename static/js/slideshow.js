1e31970697537b0bd8acf488e31c919943483f5f
James Pasko <jbpasko@gmail.com>
2013-06-08 14:55:46 -0700

    some tweaks to slideshow

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $('body > div:not(#contact-form)').on('click', function(e) {
 	var target  = $(e.target);
-	if (!target.is('#contact-link') && !target.is('#footer-logo')) {
+	if (!target.is('#contact-link') && !target.is('#footer-logo') && !target.is('#blog-link')) {
+	    $('#skip-text-display').hide();
 	    $('#background-container').fadeOut(300, function () {
 		$(this).remove();
 		$('.wrapper').css('background', $('.wrapper').data('background'));
