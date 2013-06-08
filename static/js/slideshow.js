703035147c5ba6d3976dabee6797982ab18c3903
James Pasko <jbpasko@gmail.com>
2013-06-08 10:16:46 -0700

    release slideshow feature

---
+++ static/js/slideshow.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('body > div:not(#contact-form)').on('click', function(e) {
+	var target  = $(e.target);
+	if (!target.is('#contact-link') && !target.is('#footer-logo')) {
+	    $('#background-container').fadeOut(300, function () {
+		$(this).remove();
+		$('.wrapper').css('background', $('.wrapper').data('background'));
+		$('.wrapper').css('cursor', 'auto');
+		$('#hidden-by-slideshow').show();
+	    });
+	}
+    });
+});
