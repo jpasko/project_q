dafbca9933323d20cf4d935e2083d387348860aa
James Pasko <jbpasko@gmail.com>
2013-06-08 19:49:34 -0700

    remove get-started modal, fix hidden modal on slideshow ediror

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $('body > div:not(#contact-form)').on('click', function(e) {
 	var target  = $(e.target);
-	if (!target.is('#contact-link') && !target.is('#footer-logo') && !target.is('#blog-link')) {
+	if (!target.is('#contact-link') && !target.is('#footer-logo') &&
+	    !target.is('#blog-link') && !target.is('#banner') &&
+	    !target.is('#fullname-home') && !target.is('#default-home')) {
 	    $('#skip-text-display').hide();
 	    $('#background-container').fadeOut(300, function () {
 		$(this).remove();
